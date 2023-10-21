677ec097c817745529d990370c869941442cca9f
CarlosJuanta <cjuantat@miumg.edu.gt>
2023-10-20 19:53:22 -0600

    allow cors

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const grado = require("./routes/routeGrado");
 const curso = require("./routes/routeCurso");
 const asignacionGrado = require("./routes/routeAsignacionGrado");
 
-
-
 //conectamos a la BD con la función que exportamos
 connect();
 
@@ -18,21 +16,29 @@ const app = express();
 
 //usamos cors para evitar errores de CORS
 app.use(
-    cors({
-      origin: [
-        "*",
-        "http://127.0.0.1:3000",
-        "http://localhost:3001",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-      optionsSuccessStatus: 200,
-    })
-  );
-
+  cors({
+    //add all domains of netlify
+    origin: [
+      "*",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+      "127.0.0.1:3000",
+      "https://sleepy-purse-fox.cyclic.app",
+      "https://aquamarine-pithivier-0a96ae.netlify.app",
+      "https://*.netlify.app",
+      "*.netlify.app",
+      "*.netlify.*",
+      "*.app",
+      ".app",
+      //for all sites
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hola mundo");
@@ -41,14 +47,12 @@ app.get("/", (req, res) => {
 //DEFINIR QUE USAR JSON****
 app.use(express.json());
 
-
 //RUTAS FINALES
 app.use("/api", docente);
 app.use("/api", estudiante);
 app.use("/api", grado);
 app.use("/api", curso);
 app.use("/api", asignacionGrado);
-
 
 //Iniciar el servidor
 app.listen(port, () => {
